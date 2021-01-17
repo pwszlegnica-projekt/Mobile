28d4cc4f55e02df57b935ca6c8fa1684d309a679
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-01-17 19:07:58 +0100

    added Login

---
+++ app/src/main/java/com/pwszproducts/myapplication/ui/login/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.pwszproducts.myapplication.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.pwszproducts.myapplication.R
+import com.pwszproducts.myapplication.ui.list.ListActivity
+import com.pwszproducts.myapplication.ui.products.CreateProductActivity
+
+class LoginActivity: AppCompatActivity() {
+
+    private lateinit var loginViewModel: LoginActivityViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val buttonLogin = findViewById<Button>(R.id.login)
+
+        buttonLogin.isEnabled = true
+
+        buttonLogin.setOnClickListener {
+            openNewActivity()
+        }
+
+        /*val queue = Volley.newRequestQueue(this)
+        val url = "https://www.google.com"
+
+        // Request a string response from the provided URL.
+        val stringRequest = object: StringRequest(Request.Method.GET, url,
+            { response ->
+                // Display the first 500 characters of the response string.
+                //textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            { /*textView.text = "That didn't work!"*/ }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = ""
+                //headers["Authorization"] = "Basic <<YOUR BASE64 USER:PASS>>"
+                return headers
+            }
+        }
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)*/
+    }
+
+    fun openNewActivity() {
+        val intent = Intent(this, ListActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/pwszproducts/myapplication/ui/login/LoginActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.pwszproducts.myapplication.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginActivityViewModel: ViewModel() {
+
+}
